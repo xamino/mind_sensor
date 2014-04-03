abc3d475ca5e5a9bef69ead7abced4c7b45aefa3
Tamino Hartmann <tamino.hartmann@gmail.com>
2014-04-03 19:51:54 +0200

    Implemented arguments parsing and session management

--- src/de/uulm/mi/mind/Main.java
+++ src/de/uulm/mi/mind/Main.java
@@ -1,37 +1,87 @@
 package de.uulm.mi.mind;
 
-import de.uulm.mi.mind.objects.Data;
-import de.uulm.mi.mind.objects.WifiSensor;
-import de.uulm.mi.mind.objects.enums.API;
-import de.uulm.mi.mind.objects.messages.Error;
-import de.uulm.mi.mind.objects.messages.Success;
-import de.uulm.mi.mind.remote.RemoteConnection;
+import de.uulm.mi.mind.sensor.WifiSense;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // todo parse arguments (if any?)
-        // – IP address of server!
-        // – sleep time of thread
+    private static final String HELP = "To run, start the program with these values:\n\n" +
+            "java -jar mind_sensor <server_ip> <sensor_name> <sensor_password> [<interval_seconds>]" +
+            "\n";
+    private static final String DEFAULT = "NOTE: Running program with default values! Try the option " +
+            "<help> to see what values can be set.";
+    private static final String PARSE = "Failed to parse values! Must be of format \"<name>=<value>\"!";
+    private static final String UNKNOWN = "Unknown key=value pair! Ignoring...";
 
-        RemoteConnection remoteConnection = new RemoteConnection("127.0.0.1:8080");
-        Data received = remoteConnection.runTask(API.CHECK, new WifiSensor("test", "password"), null);
-        if (received == null) {
-            System.out.println("Failed!");
+    /**
+     * Standard IP.
+     */
+    private static String ip = "127.0.0.1:8080";
+    /**
+     * Default name.
+     */
+    private static String name = "test";
+    /**
+     * Default password.
+     */
+    private static String password = "test";
+    /**
+     * Default sleep time in seconds.
+     */
+    private static int sleep = 5;
+
+    public static void main(String[] args) {
+        // check whether we can set valid parameters
+        if (!setupArguments(args)) {
             return;
         }
-        if (received instanceof de.uulm.mi.mind.objects.messages.Error) {
-            Error error = ((Error) received);
-            System.out.println(error.getType().toString() + "::" + error.getDescription());
-        } else if (received instanceof Success) {
-            System.out.println(((Success) received).getDescription());
-        } else {
-            System.out.println("Yup! " + received.toString());
-        }
-        /*
-        // todo 2 start & run scanning service
-        WifiSense sensorThread = new WifiSense(5000);
+        System.out.println("Beginning program.");
+        // start & run scanning service
+        WifiSense sensorThread = new WifiSense(ip, name, password, sleep);
         new Thread(sensorThread).run();
-        */
+        // todo make sure no memory leaks are taking place
+        // finally
+        System.out.println("Terminating program.");
+    }
+
+    /**
+     * Method for parsing command line arguments.
+     *
+     * @param args The list of arguments given to the program.
+     * @return True if we should continue executing the program, else false.
+     */
+    private static boolean setupArguments(String[] args) {
+        if (args.length == 0) {
+            System.out.println(DEFAULT);
+        }
+        // parse arguments and offer help
+        for (String argument : args) {
+            if (argument.equals("help")) {
+                System.out.println(HELP);
+                continue;
+            }
+            String[] keyValuePair = argument.split("=");
+            if (keyValuePair.length != 2) {
+                System.out.println(PARSE);
+                return false;
+            }
+            argument = keyValuePair[0];
+            String value = keyValuePair[1];
+            switch (argument) {
+                case "name":
+                    name = value;
+                    break;
+                case "password":
+                    password = value;
+                    break;
+                case "ip":
+                    ip = value;
+                    break;
+                case "interval":
+                    sleep = Integer.getInteger(value);
+                default:
+                    System.out.println(UNKNOWN);
+            }
+        }
+        return true;
     }
 }
--- src/de/uulm/mi/mind/sensor/WifiSense.java
+++ src/de/uulm/mi/mind/sensor/WifiSense.java
@@ -1,6 +1,11 @@
 package de.uulm.mi.mind.sensor;
 
 import de.uulm.mi.mind.logger.Messenger;
+import de.uulm.mi.mind.objects.Data;
+import de.uulm.mi.mind.objects.WifiSensor;
+import de.uulm.mi.mind.objects.enums.API;
+import de.uulm.mi.mind.objects.messages.Success;
+import de.uulm.mi.mind.remote.RemoteConnection;
 
 /**
  * @author Tamino Hartmann
@@ -22,15 +27,25 @@ public class WifiSense implements Runnable {
      */
     private final String TAG = "WifiSense";
 
+    private RemoteConnection connection;
+    private WifiSensor sensorUser;
+    private String session;
+
     /**
      * Constructor. Sets the sleep time, gets important instances, and prepares the connection to the server.
      *
-     * @param sleepTime The time in ms that the the thread sleeps between scans.
+     * @param ip
+     * @param name
+     * @param password
+     * @param sleepTime
      */
-    public WifiSense(long sleepTime) {
+    public WifiSense(String ip, String name, String password, int sleepTime) {
+        // get instances
         log = Messenger.getInstance();
-        // set defaults
-        SLEEP_TIME = sleepTime;
+        // set vars
+        SLEEP_TIME = sleepTime * 1000;
+        connection = new RemoteConnection(ip);
+        sensorUser = new WifiSensor(name, password);
 
         log.log(TAG, "Created.");
     }
@@ -41,7 +56,14 @@ public class WifiSense implements Runnable {
     @Override
     public void run() {
         while (true) {
-            // todo check session and if not valid, try login
+            //check session
+            if (!checkSession()) {
+                log.error(TAG, "Session could not be established!");
+                break;
+            }
+            log.log(TAG, "Beginning scan...");
+            // todo implement
+            log.log(TAG, "Finished scan.");
             try {
                 Thread.sleep(SLEEP_TIME);
             } catch (InterruptedException e) {
@@ -50,5 +72,26 @@ public class WifiSense implements Runnable {
                 e.printStackTrace();
             }
         }
+        log.log(TAG, "Terminated.");
+    }
+
+    /**
+     * This method makes sure that the sensor is logged in. If not, it will try to get a session.
+     */
+    private boolean checkSession() {
+        // start with check
+        Data data = connection.runTask(API.CHECK, null, session);
+        // if okay, return true
+        if (data instanceof Success) {
+            return true;
+        }
+        // otherwise try login
+        data = connection.runTask(API.LOGIN, sensorUser, null);
+        if (data instanceof Success) {
+            log.log(TAG, "Successfully logged in.");
+            session = ((Success) data).getDescription();
+            return true;
+        }
+        return false;
     }
 }
